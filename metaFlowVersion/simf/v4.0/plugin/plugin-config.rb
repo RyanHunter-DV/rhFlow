ce31d8ae78117ecba13fc494768e561553cd5f8d
RyanHunter-DV <ryanhunter30479@outlook.com>
2023-05-16 22:19:07 +0800

    update for new projects

--- metaFlowVersion/simf/v4.0/plugin/plugin-config.rb
+++
@@ -1,7 +0,0 @@
-plugin 'xcelium' do
-	xlm = Xcelium.new(self,Context.debug);
-    self.instance_variable_set('@xcelium',xlm);
-    self.define_singleton_method :xcelium do
-        return @xcelium;
-    end
-end
