ee1f9148e170cfcd6ba56166ff5953f553747a16
ryanhunter <ryanhunter30479@outlook.com>
2023-08-10 22:16:26 +0800

    up

--- rsim/v5.0/src/ipxact/metadata.rb
+++ rsim/v5.0/src/ipxact/metadata.rb
@@ -5,11 +5,64 @@ class.
 class MetaData
 	attr_accessor :vlnv;
 
-public
+	attr :nodes;
 	def initialize(name) ##{{{
 		@vlnv = name;
+		## format is: nodes[block.location] = &block;
+		@nodes={};
+	end ##}}}
+
+public
+	## API: push(loc,b), inject a new block with the location.
+	def push(locs,b) ##{{{
+		file = File.absolute_path(locs[0]);
+		loc = "#{file},#{locs[1]}"; # <file>,<line>
+		@nodes[loc] = b;
+	end ##}}}
+
+	## API: evalUserNodes(o), to eval the user nodes by the given object
+	def evalUserNodes(t,o) ##{{{
+		@nodes.each_pair do |loc,b|
+			begin
+				@nodeloc = loc;
+				o.instance_eval &b;
+			rescue => e
+				msg = "Error detected in #{t}.elaborate\n";
+				msg+= "node location: #{loc}\n";
+				msg+= "message: ";
+				Rsim.mp.error("#{msg}#{e.message}");
+				return Rsim::FAILED;
+			end
+		end
+		return Rsim::SUCCESS;
+	end ##}}}
+
+	## API: name(n=''), if given n is not '', then set the vlnv, else return it
+	def name(n='') ##{{{
+		return @vlnv if n=='';
+		@vlnv = n;
 	end ##}}}
 
 private
+
+	attr :nodeloc;
+
+	## API: currentNodeLocation(fmt), according to given format, return current node location
+	# fmt = :file, return file
+	# fmt = :path, return path where this node exists
+	# fmt = :full, return <file,line> location.
+	def currentNodeLocation(fmt) ##{{{
+		case(fmt)
+		when :file
+			return @nodeloc.split(',')[0];
+		when :path
+			f = @nodeloc.split(',')[0];
+			return File.dirname(f);
+		when :full
+			return @nodeloc;
+		else
+			raise "not supported format(#{fmt})";
+		end
+	end ##}}}
 
 end
