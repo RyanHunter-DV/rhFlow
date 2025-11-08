c190618a736fdae33ed4ae1e5670e2c7e22ef28f
CentOS8 <ryan@localhost.localdomain>
2025-11-07 22:12:39 -0800

    up

--- be/boot/bin/bootenv-v1.bash
+++ be/boot/bin/bootenv-v1.bash
@@ -1,3 +1,13 @@
+# Goto function to jump between labels
+goto() {
+	local label=$1
+	if declare -f "$label" > /dev/null; then
+		"$label"
+    else
+		echo "Error: Label '$label' not found" >&2
+	fi
+}
+signal=0;
 pdir=${BEHOME}/boot
 workhome=`realpath .`
 project=`basename ${workhome}`
@@ -5,6 +15,7 @@ project=`basename ${workhome}`
 ## envview=xxx
 argumentCommands=`${pdir}/accessory/__optionProcess__.rb ${SHELLTYPE} $@`
 ## echo $argumentCommands
+echo $argumentCommands;
 eval $argumentCommands
 ## echo "test envview: $envview"
 ## echo "test newterm: $newterm"
@@ -12,7 +23,9 @@ eval $argumentCommands
 envfile="${workhome}/__be__/${envview}.anchor"
 if [ ! -e ${envfile} ]; then
 	echo "the specified env file not exists ${envfile}"
-	exit 3
+	signal=3
+	echo "exit with ${signal}"
+	return $signal;
 fi
 
 ## using gnome-terminal by default, others are not supported in current version
@@ -24,6 +37,7 @@ setupcmd="source ${BEHOME}/setup.sh ${BEHOME}"
 bootcmd="source ${BEHOME}/boot/bin/__bootinNewTerminal__.${SHELLTYPE} ${envfile} ${workhome}"
 logincmd="${setupcmd};${bootcmd};${SHELLTYPE}"
 localcmd="${setupcmd};${bootcmd};"
+echo ${fullcmd};
 
 if [ ${newterm} == 1 ]; then
 	echo "booting project env with new terminal"
@@ -36,4 +50,3 @@ fi
 echo ${fullcmd}
 eval ${fullcmd}
 
-## exit 0
